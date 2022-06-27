f1b9e4b96a4b9bf3631c60632880ab24e5a4393d
NoureddineTaher <noureddine_taher@yahoo.com>
2022-06-27 10:10:02 +0200

    crud tutorials

--- spring-boot-server/src/main/java/com/nouri/spring/datajpa/repository/TutorialRepository.java
+++ spring-boot-server/src/main/java/com/nouri/spring/datajpa/repository/TutorialRepository.java
@@ -1,10 +1,10 @@
-package com.bezkoder.spring.datajpa.repository;
+package com.nouri.spring.datajpa.repository;
 
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.bezkoder.spring.datajpa.model.Tutorial;
+import com.nouri.spring.datajpa.model.Tutorial;
 
 public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
 	List<Tutorial> findByPublished(boolean published);
